75a76e7223d3440ac4b7b8331322852057ee098e
windows <windows@microsoft.com>
2023-06-15 13:42:06 +0530

    change

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const  express =require('express');
-
 const apps = express();
+const path = require('path');
+apps.use(express.static('public'));
 require('dotenv').config();
+apps.use('/static', express.static(path.join(__dirname, 'public')));
 const PORT = process.env.PORT || 2000;
 
 apps.get("/",(req,res)=>{
@@ -14,6 +16,9 @@ apps.get("/about",(req,res)=>{
 
 apps.get("/contact",(req,res)=>{
     res.send(`<h1>heading</h1><a href="http://google.com">google</a>`);
+});
+apps.get("/myform",(req,res)=>{
+    res.send('about.html');
 })
 
 
